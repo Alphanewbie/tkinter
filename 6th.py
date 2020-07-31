cede7cdc9539d217b31d7b739dda02c2f0456fc8
overmaind <overelectric@gmail.com>
2020-07-31 17:13:03 +0900

    Add 6th~7th.py

--- 6th.py
+++ 6th.py
@@ -3,11 +3,14 @@ root = Tk()
  
 def click(event):
     print("클릭위치", event.x, event.y)
- 
-lbl = Label(root, text="이름")
-lbl.grid(row=0, column=0)
+
+root.title("happy")
+
+lbl = Label(root, text="Class Notice")
+lbl.pack()
+
 frame = Frame(root, width=300, height=300)
- 
+
  #왼쪽 마우스 버튼 바인딩
 frame.bind("<Button-1>", click) 
 frame.bind("<Button-3>", click) 
